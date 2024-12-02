a12328b9f638e707de845a491ace22b87ffad537
Markus Everling <markuseverling@gmail.com>
2024-12-02 16:04:22 +0100

    day 2

--- src/day2.rs
+++ src/day2.rs
@@ -1,8 +1,47 @@
+use itertools::Itertools;
+use std::cmp::Reverse;
 
-pub fn part1(_input: &str) -> String {
-    todo!()
+struct Report {
+    levels: Vec<usize>,
 }
 
-pub fn part2(_input: &str) -> String {
-    todo!()
+fn parse_report(line: &str) -> Report {
+    Report {
+        levels: line
+            .split_ascii_whitespace()
+            .map(|i| i.parse().unwrap())
+            .collect(),
+    }
+}
+
+fn parse_input(input: &str) -> Vec<Report> {
+    input.lines().map(parse_report).collect()
+}
+
+fn is_good(seq: impl Iterator<Item = usize> + Clone) -> bool {
+    (seq.clone().is_sorted() || seq.clone().is_sorted_by_key(Reverse))
+        && seq
+            .tuple_windows()
+            .all(|(a, b)| (1..=3).contains(&a.abs_diff(b)))
+}
+
+pub fn part1(input: &str) -> String {
+    let reports = parse_input(input);
+    reports
+        .iter()
+        .filter(|r| is_good(r.levels.iter().copied()))
+        .count()
+        .to_string()
+}
+
+pub fn part2(input: &str) -> String {
+    let reports = parse_input(input);
+    reports
+        .iter()
+        .filter(|r| {
+            (0..r.levels.len())
+                .any(|i| is_good(r.levels[..i].iter().chain(&r.levels[i + 1..]).copied()))
+        })
+        .count()
+        .to_string()
 }
